be6c01eadcafc70d51edca86643f7951af9ba1e6
stefamc3 <sthepaniamurxia@gmail.com>
2021-06-04 12:55:31 -0500

    diseño del generar reporte recebera

--- QYMSAS/ReporteRecebera.Designer.cs
+++ QYMSAS/ReporteRecebera.Designer.cs
@@ -40,6 +40,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.FechaHasta = new System.Windows.Forms.DateTimePicker();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
@@ -49,8 +50,8 @@
             // 
             // pictureBox7
             // 
-            this.pictureBox7.Image = global::QYMSAS.Properties.Resources.cuenta;
-            this.pictureBox7.Location = new System.Drawing.Point(12, 159);
+            this.pictureBox7.Image = global::QYMSAS.Properties.Resources.reportar;
+            this.pictureBox7.Location = new System.Drawing.Point(12, 173);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(111, 110);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -84,7 +85,7 @@
             // pictureBox11
             // 
             this.pictureBox11.Image = global::QYMSAS.Properties.Resources.logo;
-            this.pictureBox11.Location = new System.Drawing.Point(119, 16);
+            this.pictureBox11.Location = new System.Drawing.Point(132, 16);
             this.pictureBox11.Name = "pictureBox11";
             this.pictureBox11.Size = new System.Drawing.Size(93, 80);
             this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -95,7 +96,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Lucida Handwriting", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(231, 27);
+            this.label6.Location = new System.Drawing.Point(244, 27);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(196, 36);
             this.label6.TabIndex = 48;
@@ -105,7 +106,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(266, 63);
+            this.label1.Location = new System.Drawing.Point(279, 63);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(111, 23);
             this.label1.TabIndex = 138;
@@ -116,7 +117,7 @@
             this.FechaDesde.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.FechaDesde.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FechaDesde.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FechaDesde.Location = new System.Drawing.Point(257, 159);
+            this.FechaDesde.Location = new System.Drawing.Point(257, 173);
             this.FechaDesde.Name = "FechaDesde";
             this.FechaDesde.Size = new System.Drawing.Size(314, 26);
             this.FechaDesde.TabIndex = 147;
@@ -125,7 +126,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(129, 162);
+            this.label2.Location = new System.Drawing.Point(129, 176);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(99, 23);
             this.label2.TabIndex = 148;
@@ -135,7 +136,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(129, 246);
+            this.label3.Location = new System.Drawing.Point(129, 260);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(91, 23);
             this.label3.TabIndex = 150;
@@ -146,7 +147,7 @@
             this.FechaHasta.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.FechaHasta.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FechaHasta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FechaHasta.Location = new System.Drawing.Point(257, 243);
+            this.FechaHasta.Location = new System.Drawing.Point(257, 257);
             this.FechaHasta.Name = "FechaHasta";
             this.FechaHasta.Size = new System.Drawing.Size(314, 26);
             this.FechaHasta.TabIndex = 149;
@@ -154,7 +155,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::QYMSAS.Properties.Resources.generar_reporte_boton;
-            this.pictureBox1.Location = new System.Drawing.Point(237, 334);
+            this.pictureBox1.Location = new System.Drawing.Point(225, 341);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(140, 38);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -162,12 +163,23 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Sans", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(171, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(240, 32);
+            this.label4.TabIndex = 152;
+            this.label4.Text = "Reporte Recebera";
+            // 
             // ReporteRecebera
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(583, 468);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.FechaHasta);
@@ -207,5 +219,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DateTimePicker FechaHasta;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
